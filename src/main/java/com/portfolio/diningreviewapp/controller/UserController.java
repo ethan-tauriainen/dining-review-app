468094e10ca77ee70e64a266a32c27a62f21d4a6
ethan-tauriainen <ethan.tauriainen@mykenzie.snhu.edu>
2022-07-22 13:37:17 -0400

    removed unused import statements

--- src/main/java/com/portfolio/diningreviewapp/controller/UserController.java
+++ src/main/java/com/portfolio/diningreviewapp/controller/UserController.java
@@ -4,7 +4,6 @@ import com.portfolio.diningreviewapp.model.User;
 import com.portfolio.diningreviewapp.model.UserDto;
 import com.portfolio.diningreviewapp.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
